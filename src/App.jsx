516dfa0716503e1d3028d26b4e1584e6886fc37b
Gayathri S <gayathris@Gayathris-MacBook-Pro.local>
2026-09-18 23:54:06 +0530

    Sceroll fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Events from "./pages/Events";
 import EventDetails from "./pages/EventDetails";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
+  return null;
+}
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/events" element={<Events />} />
-      <Route path="/events/:slug" element={<EventDetails />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/events/:slug" element={<EventDetails />} />
+      </Routes>
+    </>
   );
 }
